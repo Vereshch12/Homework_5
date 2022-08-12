c3ff4757839d4aa4b88b6c6c67e505a3f8cc00d1
Vereshchagin_Mikhail <farasers@mail.ru>
2022-08-12 19:59:57 +0300

    ДЗ №5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Задание №1
+        int[] arrInt = new int [3];
+        arrInt[0] = 1;
+        arrInt[1] = 2;
+        arrInt[2] = 3;
+        for (int i = 0; i < arrInt.length; i++){
+            System.out.print(arrInt[i] + " ");
+        }
+        System.out.println();
+        double[] arrDouble = {1.57, 7.654, 9.986};
+        for (int i = 0; i < arrDouble.length; i++){
+            System.out.print(arrDouble[i] + " ");
+        }
+        System.out.println();
+        boolean[] arrBoolean = {true, false, false};
+        for (int i = 0; i < arrBoolean.length; i++){
+            System.out.print(arrBoolean[i] + " ");
+        }
     }
 }
